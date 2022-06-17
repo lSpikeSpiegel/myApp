3fd88c5174e9637b785f9e3ea2bf12a6a58d28ab
lilong <15922896158@163.com>
2022-06-17 12:35:57 +0800

    monsterList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+        console.log(1)
+        this.state = {
+            monsters: [],
+            searchName: ''
+        }
+    }
+    componentDidMount() {
+        console.log(3)
+        fetch('http://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(res => {
+                this.setState(
+                    () => {
+                        return { monsters: res }
+                    },
+                    () => {
+                        console.log(this.state)
+                    }
+                )
+            })
+    }
+
+
+
+    render() {
+        console.log(2)
+
+        let renderMonsters = this.state.monsters.filter(item => {
+            return item.name.toLocaleLowerCase().includes(this.state.searchName)
+        })
+        return (
+            <>
+                <input className="search-box" type='search' placeholder='search monster'
+                    onChange={(e) => {
+                        this.setState(() => {
+                            return { searchName: e.target.value }
+                        })
+                    }} />
+                {renderMonsters.map(item => (
+                    <h1 key={item.id}>{item.name}</h1>
+                ))}
+            </>
+        )
+    }
+
 }
-
-export default App;
+export default App
